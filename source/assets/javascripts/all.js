e07717c0d36e62550bd090dcf95cc3030e911dff
Greg Greiner <greg.greiner@digitalscientists.com>
2013-03-31 10:03:59 -0400

    find an apartment search form

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,1 +1,10 @@
-//= require_tree .
+//= require lib/jquery-1.8.3.min
+//= require lib/chosen.jquery.min
+
+$(document).ready(function() {
+  
+  //init jquery chose
+  $(".chzn-select").chosen(); 
+  $(".chzn-select-deselect").chosen({allow_single_deselect:true});
+
+});
