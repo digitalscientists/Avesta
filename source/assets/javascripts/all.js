e4860b9ac0f6885e94612d921a394bed2692fb8f
Fadi Eliya <fadi.eliya@digitalscientists.com>
2013-04-05 20:22:36 -0400

    [#47323161] Loading search results via tabletop
    - created templates using underscore.js
    - loading cities into select box

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,5 +1,58 @@
 //= require lib/jquery-1.8.3.min
 //= require lib/chosen.jquery.min
+//= require lib/underscore-min
+//= require lib/tabletop
+
+var Avesta = {
+  cities: [],
+  templates: {},
+  collectTemplates: function(){
+    var self = this;
+    $("script[type='text/template']").each(function(index,item){
+      self.templates[item.getAttribute('name')] = _.template(item.innerHTML);
+    });
+  },
+  prepareData: function(){
+    var self = this;
+    this.data.forEach(function(item){
+      self.cities.push(item.city)
+      item.amenities = []; item.features = []; item.numBeds = [];
+      item.numBaths = [];  item.minRent = [];  item.maxRent = [];
+      for(var i = 1; i <= 23; i++){
+        if(item['features' + i]) item.features.push(item['features' + i]);
+        if(item['amenities' + i]) item.amenities.push(item['amenities' + i]);
+      }
+      for(var key in item){
+        if(key.match(/^bed/) && item[key]){
+          var types = key.split('-');
+          item.numBeds.push(types[1]);
+          item.numBaths.push(types[3]);
+          item[types[4] + 'Rent'].push(item[key]);
+        }
+      }
+      item.numBeds = _.uniq(item.numBeds.sort(),true)
+      item.numBaths = _.uniq(item.numBaths.sort(),true)
+      item.minRent = _.uniq(item.minRent.sort(),true)
+      item.maxRent = _.uniq(item.maxRent.sort(),true)
+    })
+    self.cities = _.uniq(self.cities.sort(),true)
+  },
+  renderResults: function(){
+    $('#search-results').html(
+      this._render('search-results',{
+        city: 'Alph',state: 'FL',results: this.data
+      })
+    )
+    $('select[name=city]').append(
+      this._render('city-options',{cities: this.cities}));
+  },
+  _render: function(template, object){
+    return this.templates[template](object);
+  },
+  initialize: function(){
+    this.collectTemplates();
+  }
+};
 
 $(document).ready(function() {
   
@@ -34,4 +87,16 @@ $(document).ready(function() {
     });
   });
 
+
+  Tabletop.init({
+    key: 'https://docs.google.com/spreadsheet/pub?key=0AuVlvDns_FoWdFJ3a3NzT2RENy01eHlta2tlcUdVM2c&',
+    callback: function(data, tabletop) {
+       Avesta.data = data;
+       Avesta.prepareData();
+       Avesta.renderResults();
+     },
+     simpleSheet: true
+  })
+
+  Avesta.initialize();
 });
